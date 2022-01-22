e775a8f6b647709eb75ea796b0dbd607c191c02a
ArtemShtepa <artemshtepa@ya.ru>
2022-01-22 15:52:59 +0300

    Add homework 1.1 and links

---
+++ homework/1.1/netology.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+STUDENT_NAME="Artem"
+
+echo "my name in ${STUDENT_NAME}"
---
+++ homework/1.1/netology.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = "us-west-2"
+  version = "~> 2.18"
+}
+
+resource "aws_cloudwatch_log_group" "lambda" {
+  name = "/aws/lambda/netology"
+
+  retention_in_days = 1
+}
+
+# Artem
